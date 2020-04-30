0d8fc4495087f04ab7aea8d06a750f8860abd71c
Nathanael Mathieu <nmathieu@wesleyan.edu>
2020-04-30 13:07:38 -0400

    Add new MainPage

---
+++ src/pages/MainPage.js
@@ -0,0 +1,28 @@
+import "./About.scss";
+import { ImageCard, Layout, Text } from "../components/";
+import React, { Component } from "react";
+import { aboutMeText } from "./Text/aboutText.js";
+import professionalPhoto from "../resources/ProfessionalPhoto.JPG";
+
+export default class About extends Component {
+  componentDidMount() {
+    document.title = "About Me | Nathanael Mathieu";
+  }
+
+  render() {
+    return (
+      <Layout>
+        <div className="About">
+          <div className="profile_picture">
+            <ImageCard alt="My professional photo"
+              size="square" image={professionalPhoto}/>
+          </div>
+          <div className="profile_text">
+            <Text class="heading" text="Nathanael Mathieu"/>
+            {aboutMeText}
+          </div>
+        </div>
+      </Layout>
+    );
+  }
+}
---
+++ src/pages/Text/mainText.js
@@ -0,0 +1,26 @@
+/* eslint-disable max-len */
+import React from "react";
+import { Text } from "../../components";
+import resume from "../../resources/Resume.pdf";
+
+export const aboutMeText = <Text preformatted=
+
+  {<>
+
+    <p>
+
+      My name is Nate and I use he/him pronouns. I am a Computer Science Major and Integrated Design, Engineering, and Applied Sciences Minor in the Class of 2020 at Wesleyan University. I am looking for an Entry-Level Python Full Stack Software Engineering Job. If you think I might be a good match for a position, check out my resume <a href = {resume} target="_blank" rel="noopener noreferrer">here</a>.
+
+    </p>
+
+    <p>
+
+        Since high school I`&apos`ve been answering the question, `&quo`What do you want to do`&quo` with `&quo`Build Stuff.`&quo` It`&apos`s not a very specific goal by any means, but it speaks to my underlying desire to create. That desire has shaped much of my life. Whether it is working on coding, cooking, baking, brewing, or building, I have devoted endless hours to craft.
+
+    </p>
+
+  </>
+
+  }
+
+/>;
