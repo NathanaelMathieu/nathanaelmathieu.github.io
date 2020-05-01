a5eb146d7c7863e52c9ebb2f10da16dd59fa4c1e
Nathanael Mathieu <nmathieu@wesleyan.edu>
2020-05-01 18:43:01 -0400

    Switch projects to a function

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -11,39 +11,35 @@ import multi from "../resources/projects/pacman/multi.gif";
 import side from "../resources/projects/tag/side.jpg";
 
 
-export default class Projects extends Component {
-  componentDidMount() {
-    document.title = "Projects | Nathanael Mathieu";
-  }
-
-  render() {
-    return (
-        <div className="Projects">
-          <ImageCard route="template" image={contact}
-           textEnabled clickable alt="Click to visit my Cube project page"
-           text="IDEAS Portfolio Template | Spring 2020"/>
-          <ImageCard route="basinski" image={b3} classname="alt-text-color"
-           textEnabled clickable alt="Click to visit my Cube project page"
-           text="Basinski | Fall 2019"/>
-          <ImageCard image={cubeIso1} route="cube"
-           textEnabled clickable alt="Click to visit my Cube project page"
-           text="Cube | Fall 2019"/>
-          <ImageCard route="pacman" image={multi} classname="alt-text-color"
-           textEnabled clickable alt="Click to visit my Cube project page"
-           text="Pacman Project | Fall 2019" />
-           <ImageCard route="tempist" image={login}
-           textEnabled clickable alt="Click to visit my Cube project page"
-           text="Tempist | Spring 2019" />
-          <ImageCard route="hopper" image={final}
-           textEnabled clickable alt="Click to visit my Cube project page"
-           text="Hopper | Fall 2018"/>
-          <ImageCard route="marble" image={kids}
-            textEnabled clickable alt="Click to visit my Cube project page"
-            text="Marble Run | Fall 2018"/>
-          <ImageCard route="tag" image={side}
-           textEnabled clickable alt="Click to visit my Cube project page"
-           text="Robot Tag | Spring 2018"/>
-        </div>
-    );
-  }
+function Projects() {
+  return (
+    <div className="Projects">
+      <ImageCard route="template" image={contact}
+        textEnabled clickable alt="Click to visit my Cube project page"
+        text="IDEAS Portfolio Template | Spring 2020"/>
+      <ImageCard route="basinski" image={b3} classname="alt-text-color"
+        textEnabled clickable alt="Click to visit my Cube project page"
+        text="Basinski | Fall 2019"/>
+      <ImageCard image={cubeIso1} route="cube"
+        textEnabled clickable alt="Click to visit my Cube project page"
+        text="Cube | Fall 2019"/>
+      <ImageCard route="pacman" image={multi} classname="alt-text-color"
+        textEnabled clickable alt="Click to visit my Cube project page"
+        text="Pacman Project | Fall 2019" />
+        <ImageCard route="tempist" image={login}
+        textEnabled clickable alt="Click to visit my Cube project page"
+        text="Tempist | Spring 2019" />
+      <ImageCard route="hopper" image={final}
+        textEnabled clickable alt="Click to visit my Cube project page"
+        text="Hopper | Fall 2018"/>
+      <ImageCard route="marble" image={kids}
+        textEnabled clickable alt="Click to visit my Cube project page"
+        text="Marble Run | Fall 2018"/>
+      <ImageCard route="tag" image={side}
+        textEnabled clickable alt="Click to visit my Cube project page"
+        text="Robot Tag | Spring 2018"/>
+    </div>
+  );
 }
+
+export default Projects;
