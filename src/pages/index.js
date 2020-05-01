2500631b466fa1bfb07e6d776f08959753fa615d
Nathanael Mathieu <nmathieu@wesleyan.edu>
2020-05-01 18:41:09 -0400

    Remove unnecessary pages and background, update exports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,3 @@
 export { default as Projects } from "./Projects.js";
 export { default as NotFound } from "./NotFound.js";
 export { default as MainPage } from "./MainPage.js";
-export { default as Contact } from "./Contact.js";
-export { default as About } from "./About.js";
