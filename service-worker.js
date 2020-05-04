e0cee7aee2a33564055a92aaa20f4a44c9ecf9d2
Nathanael Mathieu <nmathieu@wesleyan.edu>
2020-05-03 22:03:44 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://nathanaelmathieu.github.io/pro-site/precache-manifest.22d61c68d1ef8d75887c5cd21f652240.js"
+  "https://nathanaelmathieu.github.io/pro-site/precache-manifest.a6e52d50a132e9622e556f2fee36c3ad.js"
 );
 
 self.addEventListener('message', (event) => {
