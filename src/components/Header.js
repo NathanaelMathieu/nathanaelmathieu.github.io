1d96dcaa78b86b825013b101321e3bf833c820e7
Nathanael Mathieu <nmathieu@wesleyan.edu>
2020-05-12 14:45:56 -0400

    use MediaQuery to only render header on mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,19 @@
 import "./Header.scss";
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import MediaQuery from "react-responsive";
 import { Text } from "./";
 
 export default class Header extends Component {
   render() {
     return (
-      <div className="Header">
-        <Link to="/">
-          <Text text="< Back" class="heading transparent-theme"/>
-        </Link>
-      </div>
+      <MediaQuery maxDeviceWidth={600}>
+        <div className="Header">
+          <Link to="/">
+            <Text text="< Back" class="heading transparent-theme"/>
+          </Link>
+        </div>
+      </MediaQuery>
     );
   }
 }
