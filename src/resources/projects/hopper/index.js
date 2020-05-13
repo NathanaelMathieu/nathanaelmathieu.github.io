8d50f21ecaeefc939e98e6b4459676aec9807821
Nathanael Mathieu <nmathieu@wesleyan.edu>
2020-05-12 21:25:43 -0400

    Remove orphaned project images

--- src/resources/projects/hopper/index.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable camelcase */
-
-export { default as final } from "./final.jpg";
-export { default as finalExt } from "./finalExt.jpg";
-export { default as holder } from "./holder.jpg";
-export { default as pendulums } from "./pendulums.jpg";
--- src/resources/projects/tag/index.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable camelcase */
-
-export { default as cageIso } from "./cageIso.jpg";
-export { default as side } from "./side.jpg";
-export { default as cardboard } from "./cardboard.jpg";
