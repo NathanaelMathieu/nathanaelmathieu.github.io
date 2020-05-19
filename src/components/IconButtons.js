86aaf25679480f3217079d22584997cf4bb53acc
Nathanael Mathieu <nmathieu@wesleyan.edu>
2020-05-19 15:01:03 -0400

    Update resume

--- src/components/IconButtons.js
+++ src/components/IconButtons.js
@@ -5,7 +5,7 @@ import { FaLinkedin } from "react-icons/fa";
 import { GoMarkGithub } from "react-icons/go";
 import { IconContext } from "react-icons";
 import React from "react";
-import resume from "../Resume.pdf";
+import resume from "../Nathanael_Mathieu_Resume.pdf";
 
 function styleIcon(IconComponent, route) {
   return (
